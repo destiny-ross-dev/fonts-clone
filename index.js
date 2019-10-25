9eba9613cd26281480165ba6df358db704fc87d3
Destiny Ross <destinyross.dev@gmail.com>
2019-10-24 21:29:37 -0400

    restructure

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ globalMiddleware(app);
 app.use("/fonts", fontsRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("/*", function(req, res) {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
   });
 }
 
